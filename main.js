6516684175b9a4a65c8cf5fafa6643fef6860439
agan-k <koranagan@gmail.com>
2020-04-27 11:05:38 -0400

    Revert "Ka mobile nav"

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-let toggle = document.querySelector('#burger');
-let sidebar = document.querySelector('nav');
-
-toggle.addEventListener('click', function(e) {
-    e.preventDefault();
-    sidebar.classList.toggle('open');
-});
+ 
